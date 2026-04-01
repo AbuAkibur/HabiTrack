d01ff2c39214e3a4ea363b58033b0fd5556a4cfa
AbuAkibur <abuakibur@gmail.com>
2026-04-02 02:15:10 +0600

    Add HabitsContext for global state management

---
+++ src/context/HabitsContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext } from 'react'
+import { useHabits } from '../hooks/useHabits'
+
+const HabitsContext = createContext(null)
+
+export function HabitsProvider({ children }) {
+  const value = useHabits()
+  return (
+    <HabitsContext.Provider value={value}>
+      {children}
+    </HabitsContext.Provider>
+  )
+}
+
+export function useHabitsContext() {
+  const ctx = useContext(HabitsContext)
+  if (!ctx) throw new Error('useHabitsContext must be used inside HabitsProvider')
+  return ctx
+}
